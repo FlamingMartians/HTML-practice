b96c1a0d86f2994a29d43a430adb7c4a8f8615d7
George Shao <flamingmartians@gmail.com>
2021-09-25 13:44:07 -0700

    Updated

--- pythonPractice.py
+++ pythonPractice.py
@@ -1,12 +1,37 @@
-# This is a Python comment
-names = ["Harry", "Ron", "Hermione"]
-# Print the entire list:
-print(names)
-# Print the second element of the list:
-print(names[1])
-# Add a new name to the list:
-names.append("Draco")
-# Sort the list:
-names.sort()
-# Print the new list:
-print(names)
+from pyFunctions import square
+
+class Flight():
+    # Method to create new flight with given capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    # Method to add a passenger to the flight:
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    # Method to return number of open seats
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+# Create a new flight with o=up to 3 passengers
+flight = Flight(3)
+people = []
+
+# Create a list of people
+print("Please input a list of passengers, input \"done\" once finished.")
+passenger = input()
+while(passenger != "done"):
+	people.append(passenger)
+	passenger = input()
+
+
+# Attempt to add each person in the list to a flight
+for person in people:
+    if flight.add_passenger(person):
+        print(f"Added {person} to flight successfully")
+    else:
+        print(f"No available seats for {person}")
